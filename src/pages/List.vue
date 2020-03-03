140a1f8c452fffc618e6c37d84e02e4e6ce4cd1e
60160199 <60160199@users.noreply.github.com>
2020-03-03 17:45:08 +0700

    Update List.vue

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -1,10 +1,14 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-    <h3>List workshjhjhjjj</h3>
-  </q-page>
-</template>
+<q-card class="my-card">
+      <q-card-section>
+        <div class="row">
+        <div class="col-5">วันนี้ 19/02/20</div>
+        <div class="col-3">1000.50 บาท</div>
+        <div class="col-3">-100.00 บาท</div>
+        </div>
+      </q-card-section>
 
+      <q-separator />
 
 
 <!-- AOM -->
