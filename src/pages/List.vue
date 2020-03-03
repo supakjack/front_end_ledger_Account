2a04b6ec9c71b6da38af2e508f0724658815f86d
Pornpailin9 <61728776+Pornpailin9@users.noreply.github.com>
2020-03-03 17:18:03 +0700

    Update List.vue

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -1,12 +1,32 @@
+<!-- นางสาวพรไพลิน กล่อมจันทร์ [start] หน้า view ของหน้าที่สอง -->
+
 <template>
-  <q-page padding>
-    <!-- content -->
-    <h3>List workshjhjhjjj</h3>
-  </q-page>
+  <div class="q-pa-md row items-start q-gutter-md ">
+    <q-card class="my-card">
+        <div class="row ">
+          <div class="col-5">สรุปรายรับ - รายจ่าย</div>
+          <div class="col-3 text-blue">1000.50 บาท</div>
+          <div class="col-3 text-red">-100.00 บาท</div>
+        </div>
+
+      <!-- <q-card-section>
+        <p>สรุปรายรับ - รายจ่าย</p> <p class="text-blue">1000.50 บาท</p> <p class="text-red">-100.00 บาท</p>'
+      </q-card-section> -->
+
+    </q-card>
+
+    <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+    </q-card>
+  </div>
 </template>
 
-<script>
-export default {
-  // name: 'PageName',
-}
-</script>
+<style lang="sass" scoped>
+.my-card
+  width: 150%
+  max-width: 376px
+</style>
+
+<!-- นางสาวพรไพลิน กล่อมจันทร์ [end] หน้า view ของหน้าที่สอง  -->
+
