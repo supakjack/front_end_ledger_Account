9da738785816fc36b3c241e0f71c0ef8637974b6
fondao sriworasa <60160342@go.buu.ac.th>
2020-03-10 23:08:31 +0700

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,25 +1,52 @@
-
-const routes = [
-  {
+const routes = [{
     path: '/',
-    component: () => import('layouts/GlobalLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: 'home', component: () => import('pages/Home.vue') },
-      { path: 'authen', component: () => import('pages/Authen.vue') },
-      { path: 'account', component: () => import('pages/Account.vue') },
-      { path: 'list', component: () => import('pages/List.vue') },
-      { path: 'input', component: () => import('pages/Input.vue') },
+    component: () =>
+        import ('layouts/GlobalLayout.vue'),
+    children: [{
+            path: '',
+            component: () =>
+                import ('pages/Index.vue')
+        },
+        {
+            path: 'home',
+            component: () =>
+                import ('pages/Home.vue')
+        },
+        {
+            path: 'authen',
+            component: () =>
+                import ('pages/Authen.vue')
+        },
+        {
+            path: 'account',
+            component: () =>
+                import ('pages/Account.vue')
+        },
+        {
+            path: 'list',
+            component: () =>
+                import ('pages/List.vue')
+        },
+        {
+            path: 'income',
+            component: () =>
+                import ('pages/Income.vue')
+        },
+        {
+            path: 'expenses',
+            component: () =>
+                import ('pages/expenses.vue')
+        },
     ],
-  }
-]
+}]
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    routes.push({
+        path: '*',
+        component: () =>
+            import ('pages/Error404.vue')
+    })
 }
 
 export default routes
