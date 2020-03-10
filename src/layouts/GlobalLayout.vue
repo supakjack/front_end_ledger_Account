9351f18f540c4d2b55ba8d7f3dfa0b6301c5e4ea
fondao sriworasa <60160342@go.buu.ac.th>
2020-03-10 23:08:25 +0700

    Update GlobalLayout.vue

--- src/layouts/GlobalLayout.vue
+++ src/layouts/GlobalLayout.vue
@@ -56,8 +56,19 @@
 
             <q-item-section>หน้าบัญชี</q-item-section>
           </q-item>
+
+           <q-item exact to="/authen" active clickable v-ripple>
+            <q-item-section avatar>
+              <q-img  width="25px" src="https://image.flaticon.com/icons/svg/2635/2635961.svg" />
+            </q-item-section>
+
+            <q-item-section class="text-red">ออกจากบัญชี</q-item-section>
+          </q-item>
+
         </q-list>
       </q-scroll-area>
+
+      
 
       <div class="absolute-top bg-cyan row items-center" style="height: 150px;">
         <!-- <div class="bg-red"> -->
