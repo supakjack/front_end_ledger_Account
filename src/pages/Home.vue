4b336960030f6fad3e688c630a78b71cee5571bd
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-03-10 19:07:59 +0700

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,38 @@
 <template>
   <q-page padding>
     <!-- content -->
+    
+    <div class="q-pa-md">
+      <div class="column" style="height: 150px">
+        <div class="col">
+          <center>
+            <b style="font-size:25px; text-align:center;">ยอดรวมเงินทั้งหมด</b>
+          </center>
+        </div>
+        <div class="col">
+          <center>
+            <b style="font-size:18px; text-align:center;">{{total}} บาท</b>
+          </center>
+        </div>
+      </div>
+      <center>
+        <canvas id="myChart" height="100vh" width="130vw"></canvas>
+      </center>
+
+      <div class="row" style="font-size:20px;">
+        <div class="col" style="color:red; text-align:center;">
+          <b>รายจ่าย</b>
+        </div>
+        <div class="col" style="color:blue; text-align:center;">
+          <b>รายรับ</b>
+        </div>
+      </div>
+
+      <div class="row" style="font-size:15px;">
+        <div class="col" style="color:red; text-align:center;">{{expense}} บาท</div>
+        <div class="col" style="color:blue; text-align:center;">{{Income}} บาท</div>
+      </div>
+    </div>
     <div class="q-pa-md q-gutter-sm">
       <q-banner class="bg-grey-3">
         <p style="text-align:left;"></p>
@@ -37,38 +69,6 @@
       <div align="center">
         <q-btn size="12px" class="q-px-xl q-py-xs" color="red" label="รายจ่าย" />
         <q-btn size="12px" class="q-px-xl q-py-xs" color="green" label="รายรับ" />
-      </div>
-    </div>
-
-    <div class="q-pa-md">
-      <div class="column" style="height: 150px">
-        <div class="col">
-          <center>
-            <b style="font-size:25px; text-align:center;">ยอดรวมเงินทั้งหมด</b>
-          </center>
-        </div>
-        <div class="col">
-          <center>
-            <b style="font-size:18px; text-align:center;">{{total}} บาท</b>
-          </center>
-        </div>
-      </div>
-      <center>
-        <canvas id="myChart" height="100vh" width="130vw"></canvas>
-      </center>
-
-      <div class="row" style="font-size:20px;">
-        <div class="col" style="color:red; text-align:center;">
-          <b>รายจ่าย</b>
-        </div>
-        <div class="col" style="color:blue; text-align:center;">
-          <b>รายรับ</b>
-        </div>
-      </div>
-
-      <div class="row" style="font-size:15px;">
-        <div class="col" style="color:red; text-align:center;">{{expense}} บาท</div>
-        <div class="col" style="color:blue; text-align:center;">{{Income}} บาท</div>
       </div>
     </div>
   </q-page>
