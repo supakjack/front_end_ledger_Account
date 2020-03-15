d4c480299882458d66f98e4d12291ae2b3937570
GSGunN <drift_king_gun@hotmail.com>
2020-03-15 18:22:49 +0700

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -77,14 +77,13 @@
 <script>
 import Chart from "chart.js";
 import FacadeServices from "./../services/FacadeServices";
-import AxiosServices from "./../services/AxiosServices";
+const axios = new FacadeServices().makeAxios();
+
 export default {
   name: "Home",
   mounted() {
     
-    console.log(new AxiosServices().getHttp('books/1'))
-
-    new AxiosServices().getHttp("books/amount/2").then(result => {
+    new axios().getHttp("books/amount/2").then(result => {
       result.data.map((item,index)=>{
         this.total = item.amount
       })
