294489be62d54dfcf399de2851972ade9a0929fe
60160199 <60160199@users.noreply.github.com>
2020-03-16 19:19:17 +0700

    Update List.vue

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -3,7 +3,7 @@
   <div class="q-pa-md row items-start q-gutter-md">
     <q-card class="my-card">
       <div class="row top-center">
-        <div class="col-5 text-center" >สรุปรายรับ - รายจ่าย</div>
+        <div class="col-5 text-center">สรุปรายรับ - รายจ่าย</div>
         <div class="col-3 text-blue">{{total}}{{currency}}</div>
         <div class="col-3 text-red">{{expense}}{{currency}}</div>
       </div>
@@ -16,9 +16,7 @@
             <div class="row top-center">
               <q-item-label></q-item-label>
               <div class="col-1">
-                <q-img
-                  src="https://image.flaticon.com/icons/svg/639/639365.svg"
-                />
+                <q-img src="https://image.flaticon.com/icons/svg/639/639365.svg" />
               </div>
               <div class="col-5 text-center text-blue" style="font-size:18px">รายรับ</div>
               <div class="col-5 text-center" style="font-size:18px">{{income}}{{currency}}</div>
@@ -31,9 +29,7 @@
             <div class="row top-center">
               <q-item-label></q-item-label>
               <div class="col-1">
-                <q-img
-                  src="https://image.flaticon.com/icons/svg/1086/1086741.svg"
-                />
+                <q-img src="https://image.flaticon.com/icons/svg/1086/1086741.svg" />
               </div>
               <div class="col-5 text-center text-red" style="font-size:18px">รายจ่าย</div>
               <div class="col-5 text-center" style="font-size:18px">{{expense}}{{currency}}</div>
@@ -43,16 +39,14 @@
       </q-list>
     </q-btn-dropdown>
 
-     <q-btn-dropdown label="ข้อมูลรายรับ - รายจ่ายในวันที่ 09/03/2020">
+    <q-btn-dropdown label="ข้อมูลรายรับ - รายจ่ายในวันที่ 09/03/2020">
       <q-list>
         <q-item clickable v-close-popup @click="onItemClick()">
           <q-item-section>
             <div class="row top-center">
               <q-item-label></q-item-label>
               <div class="col-1">
-                <q-img
-                  src="https://image.flaticon.com/icons/svg/639/639365.svg"
-                />
+                <q-img src="https://image.flaticon.com/icons/svg/639/639365.svg" />
               </div>
               <div class="col-5 text-center text-blue" style="font-size:18px">รายรับ</div>
               <div class="col-5 text-center" style="font-size:18px">{{income}}{{currency}}</div>
@@ -65,9 +59,7 @@
             <div class="row top-center">
               <q-item-label></q-item-label>
               <div class="col-1">
-                <q-img
-                  src="https://image.flaticon.com/icons/svg/1086/1086741.svg"
-                />
+                <q-img src="https://image.flaticon.com/icons/svg/1086/1086741.svg" />
               </div>
               <div class="col-5 text-center text-red" style="font-size:18px">รายจ่าย</div>
               <div class="col-5 text-center" style="font-size:18px">{{expense}}{{currency}}</div>
@@ -76,8 +68,6 @@
         </q-item>
       </q-list>
     </q-btn-dropdown>
-
-
   </div>
 </template>
 
@@ -110,8 +100,18 @@ export default {
 
 <script>
 import $store from "../store/State";
+import FacadeServices from "./../services/FacadeServices";
+const axios = new FacadeServices().makeAxios();
 
 export default {
+  mounted() {
+    new axios().getHttp("books/income/1/now").then(result => {
+      result.data.map((item, index) => {
+        console.log(item);
+        this.total = item.amount;
+      });
+    });
+  },
   data() {
     return {
       total: "500.25",
@@ -122,6 +122,7 @@ export default {
       currency: "บาท",
       value: 10
     };
-  }
+  },
+  methods: {}
 };
 </script>
